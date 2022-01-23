6ec0f683c421f32ce8fd32724c4e405f0951816f
Dedrie <dedrie.peart@gmail.com>
2022-01-23 04:33:24 -0500

    psuedocode

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+//start button
+//display question 1
+    // tell user whether the answer was right or wrong
+    //proceed to next question
+// display question 2
+//dislay question 3
+//display question 4
+//disp;lay question 5
