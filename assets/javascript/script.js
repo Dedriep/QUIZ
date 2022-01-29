7e7979981dccbd935cd48e3eb1dc4d088bb290fa
Dedrie <dedrie.peart@gmail.com>
2022-01-29 11:15:59 -0500

    changed html style and updated displaycontent function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,54 +2,8 @@
 var buttonEl = document.querySelector("#button");
 var questionEl = document.querySelector("#question-header");
 var answerEl = document.querySelectorAll(".answer")
-var counter = 0
-var score = ""
-
-
-buttonEl.addEventListener("click", function () {
-  alert("The quiz has started");
-  displayContent()
-
-});
-for (var i = 0; i < answerEl.length; i++) {
-  answerEl[i].addEventListener("click", function (e) {
-    alert(e.target.value)  //process answer increment counter to next question
-    counter += 1
-    displayContent()
-  })
-}
-
-
-
-function displayContent() {
-  document.getElementById("qOne").style.display = "block";
-  questionEl.textContent = questions[counter].question;
-  answerEl.textContent = questions[counter].options;
-  document.getElementsByClassName("start-button").style.display = ""
-
-
-  for (var i = 0; i < answerEl.length; i++) {
-    if (i === questions[i].answer) {
-      answerEl[i].value = "Correct";
-      score += 10
-
-    } else {
-      answerEl[i].value = "Wrong";
-      score -= 5;
-    }
-  }
-}
-
-
-// lets user know that answer was right or wrong
-
-
-
-
-
-
-//  function w/ ARRAY TO HOLD QUESTIONS
-
+var counter = 0;
+var score = "";
 
 const questions =
   [
@@ -86,8 +40,80 @@ const questions =
 
 
 
+buttonEl.addEventListener("click", function (event) {
+  event.preventDefault(); 
+  alert("The quiz has started");
+  displayContent()
+  startClock(); 
+});
+
+function startClock(){
+
+}
+
+//for (var i = 0; i < answerEl.length; i++) {
+//answerEl[i].addEventListener("click", function (event) {
+function validateANs(event) {
+  event.preventDefault();
+  alert(event.target.value, "Button clicked")  //process answer increment counter to next question
+
+  if (event.target.value === questions[counter].answer) {
+   document.getElementById("feedback").textContent = "Correct";
+    score += 10
+
+  } else {
+    document.getElementById("feedback").textContent = "Wrong";
+    score -= 5;
+  }
+  //Move to next question 
+  counter += 1
+  displayContent();
+}
+
+
+
+const questionsList = questions.options.split('');
+
+
+function displayContent() {
+  document.getElementById("qOne").style.display = "block";
+  questionEl.textContent = questions[counter].question;
+  answerEl.textContent = questions[counter].options;
+  buttonEl.style.display = "none"
+  console.log("answerEl", answerEl);
+
+  //reset the feedback element to blank text 
+  document.getElementById("feedback").textContent = ""; 
+
+  for (var i = 0; i < answerEl.length; i++) {
+    //display text on the radio buttons 
+    console.log(answerEl[i]);
+    //Display text of the button 
+    answerEl[i].textContent = questions[counter].options[i];
+    answerEl[i].value = questions[counter].options[i];
+    //Add event listiner 
+    answerEl[i].addEventListener("click", validateANs);
+
+  }
+
+}
+
+
+// lets user know that answer was right or wrong
+
+
+
+
+
+
+//  function w/ ARRAY TO HOLD QUESTIONS
+
+
+
 //display question
 // tell user whether the answer was right or wrong
 // log points for correct answer
 //proceed to next question
 // let user know score
+
+localStorage.setItem("player")
